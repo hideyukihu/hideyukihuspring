16a7993133aa4d4aee02b22ea93c90a67d738cfb
hideyuki <majihaik@gmail.ccom>
2023-01-30 20:07:51 +0900

    first commit

---
+++ src/main/java/hideyukihu/spring/hideyukihu/HideyukihuApplication.java
@@ -0,0 +1,13 @@
+package hideyukihu.spring.hideyukihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HideyukihuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HideyukihuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hideyukihu/spring/hideyukihu/HideyukihuApplicationTests.java
@@ -0,0 +1,13 @@
+package hideyukihu.spring.hideyukihu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HideyukihuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
